48f5f5270593df937dda0aa96cd73a8e6e71403b
ionivetech <ionivetech@gmail.com>
2024-02-05 12:24:09 +0700

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+interface Props {
+  label?: string
+  variant?: string
+  iconPosition?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  variant: 'primary'
+})
+</script>
+
+<template>
+  <button :class="[`btn btn--${variant}`, { 'btn--icon-right': iconPosition === 'right' }]">
+    <div
+      class="btn__icon"
+      :class="!!iconPosition && `btn__icon--${iconPosition}`"
+    >
+      <slot name="icon" />
+    </div>
+    <span class="btn__text">{{ label }}</span>
+  </button>
+</template>
